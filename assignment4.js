1cb8f951cb03ae5e8951619410e372d7bdc52a26
fadallalaaisha <79199476+fadallalaaisha@users.noreply.github.com>
2021-03-21 11:54:54 +0300

    Add files via upload

---
+++ assignment4.js
@@ -0,0 +1,27 @@
+          getGrade() 
+function getGrade(marks){
+    var marks=84;
+    if(marks>=80)
+    {
+        console.log('A');
+    }
+     else if(marks>=70)
+    {
+        console.log('B')
+    }
+     else if (marks>=60)
+    {
+        console.log('C')
+    }
+else if (marks>=50)
+    {
+        console.log('D')
+    }
+else if(marks>=40)
+    {
+        console.log('E')
+    }
+    else {
+        console.log(F)
+    }
+} 
